bc1f3374b2899f773192f947df8066e7b50b6b73
ralph <ralph@paradata.io>
2015-12-23 01:12:04 -0800

    Navigation JS - mark the active page

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -8,6 +8,7 @@
  global = {
      init: function() {
          global.initPlugins();
+         global.getMainNav();
      },
      // INIT PLUGINS
      ///////////////
@@ -17,6 +18,14 @@
              FastClick.attach(document.body);
          });
      },
+     getMainNav: function() {
+        // Get list array of classNames, then split so only the current body className is listed
+        var classList = $('body').attr('class').split(/\s+/);
+        $.each( classList, function(index, bodyClass){
+            // loop through each bodyClass and add active to all matching elements.
+            $(".main-nav ul li a[data-page-id='"+ bodyClass +"']").addClass("active");
+        });
+     }
 };
  $(global.init);
 
